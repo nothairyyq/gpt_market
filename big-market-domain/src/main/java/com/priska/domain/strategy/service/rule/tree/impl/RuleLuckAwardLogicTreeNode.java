c435d0ead4e0c0ab80126cd8fa5157effcc31fb3
nothairyyq <yaqiyu@uw.edu>
2024-10-29 22:32:33 -0700

    规则树-兜底奖品逻辑完善

--- big-market-domain/src/main/java/com/priska/domain/strategy/service/rule/tree/impl/RuleLuckAwardLogicTreeNode.java
+++ big-market-domain/src/main/java/com/priska/domain/strategy/service/rule/tree/impl/RuleLuckAwardLogicTreeNode.java
@@ -3,6 +3,7 @@ package com.priska.domain.strategy.service.rule.tree.impl;
 import com.priska.domain.strategy.model.valobj.RuleLogicCheckTypeVO;
 import com.priska.domain.strategy.service.rule.tree.ILogicTreeNode;
 import com.priska.domain.strategy.service.rule.tree.factory.DefaultTreeFactory;
+import com.priska.types.common.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +21,28 @@ public class RuleLuckAwardLogicTreeNode implements ILogicTreeNode {
 
     @Override
     public DefaultTreeFactory.TreeActionEntity logic(String userId, Long strategyId, Integer awardId, String ruleValue) {
-        return null;
+        log.info("规则过滤-兜底奖品 userId:{} strategyId:{} awardId:{} ruleValue:{}", userId, strategyId, awardId, ruleValue);
+
+        //ruleValue: 101:1,100 split = ["101","1,100"]
+        String[] split = ruleValue.split(Constants.COLON);
+        log.info("测试的split数组:{}",split);
+        if (split.length == 0){
+            log.error("规则过滤-兜底奖品，兜底奖品未配置告警 userId:{} strategyId:{} awardId:{}", userId, strategyId, awardId);
+            throw new RuntimeException("兜底奖品未配置"+ruleValue);
+        }
+
+        //将兜底奖品id和 ruleValue值拆出来
+        Integer luckAwardId = Integer.valueOf(split[0]);
+        String awardRuleValue = split.length > 1 ? split[1] : "";
+        log.info("规则过滤-兜底奖品 userId:{} strategyId:{} awardId:{} awardRuleValue:{}", userId, strategyId, luckAwardId, awardRuleValue);
+
+        //返回兜底奖品
+        return DefaultTreeFactory.TreeActionEntity.builder()
+                .ruleLogicCheckTypeVO(RuleLogicCheckTypeVO.TAKE_OVER)
+                .strategyAwardVO(DefaultTreeFactory.StrategyAwardVO.builder()
+                        .awardId(luckAwardId)
+                        .awardRuleValue(awardRuleValue)
+                        .build())
+                .build();
     }
 }
