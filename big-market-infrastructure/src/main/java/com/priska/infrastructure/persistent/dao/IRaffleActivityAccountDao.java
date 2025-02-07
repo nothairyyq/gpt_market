e070cbc0aa4db8ab7e9bc5485830160920d6219d
nothairyyq <yaqiyu@uw.edu>
2025-02-07 15:09:18 +0800

    feat: add activity account insert

--- big-market-infrastructure/src/main/java/com/priska/infrastructure/persistent/dao/IRaffleActivityAccountDao.java
+++ big-market-infrastructure/src/main/java/com/priska/infrastructure/persistent/dao/IRaffleActivityAccountDao.java
@@ -14,4 +14,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface IRaffleActivityAccountDao {
     int updateAccountQuota(RaffleActivityAccount raffleActivityAccount);
+
+    void insert(RaffleActivityAccount raffleActivityAccount);
 }
