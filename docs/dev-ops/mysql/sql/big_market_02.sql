cd30d70421f00233c7da3af4b5eb1328828f3224
nothairyyq <yaqiyu@uw.edu>
2025-02-07 15:48:27 +0800

    update: sql structure, add bizNo and some testing data

--- docs/dev-ops/mysql/sql/big_market_02.sql
+++ docs/dev-ops/mysql/sql/big_market_02.sql
@@ -151,7 +151,45 @@ CREATE TABLE `raffle_activity_order_003` (
                                              KEY `idx_user_id_activity_id` (`user_id`,`activity_id`,`state`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='抽奖活动单';
 
+ALTER TABLE raffle_activity_order_000
+    MODIFY COLUMN state varchar(16) NOT NULL DEFAULT 'complete' COMMENT '订单状态(complete)',
+    add column out_business_no varchar(64) null COMMENT '业务防重ID - 外部透传，确保幂等' AFTER state,
+    ADD UNIQUE KEY uq_out_business_no(out_business_no);
+ALTER TABLE raffle_activity_order_000
+    MODIFY column out_business_no varchar(64) NOT null COMMENT '业务防重ID - 外部透传，确保幂等' AFTER state;
+ALTER TABLE raffle_activity_order_001
+    MODIFY COLUMN state varchar(16) NOT NULL DEFAULT 'complete' COMMENT '订单状态(complete)',
+    add column out_business_no varchar(64) NOT NULL COMMENT '业务防重ID - 外部透传，确保幂等' after state,
+    ADD UNIQUE KEY uq_out_business_no(out_business_no);
 
+ALTER TABLE raffle_activity_order_002
+    MODIFY COLUMN state varchar(16) NOT NULL DEFAULT 'complete' COMMENT '订单状态(complete)',
+    add column out_business_no varchar(64) NOT NULL COMMENT '业务防重ID - 外部透传，确保幂等 'after state,
+    ADD UNIQUE KEY uq_out_business_no(out_business_no);
+
+ALTER TABLE raffle_activity_order_003
+    MODIFY COLUMN state varchar(16) NOT NULL DEFAULT 'complete' COMMENT '订单状态(complete)',
+    add column out_business_no varchar(64) NOT NULL COMMENT '业务防重ID - 外部透传，确保幂等' after state,
+    ADD UNIQUE KEY uq_out_business_no(out_business_no);
+
+LOCK TABLES `raffle_activity_order_001` WRITE;
+/*!40000 ALTER TABLE `raffle_activity_order_001` DISABLE KEYS */;
+INSERT INTO `raffle_activity_order_001` (`id`, `user_id`, `sku`, `activity_id`, `activity_name`, `strategy_id`, `order_id`, `order_time`, `total_count`, `day_count`, `month_count`, `state`, `out_business_no`, `create_time`, `update_time`)
+VALUES
+    (3,'priska',9011,100301,'测试活动',100006,'383240888158',now(),1,1,1,'completed','700091009111',now(),now());
+/*!40000 ALTER TABLE `raffle_activity_order_001` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+LOCK TABLES `raffle_activity_account` WRITE;
+/*!40000 ALTER TABLE `raffle_activity_account` DISABLE KEYS */;
+
+INSERT INTO `raffle_activity_account` (`id`, `user_id`, `activity_id`, `total_count`, `total_count_surplus`, `day_count`, `day_count_surplus`, `month_count`, `month_count_surplus`, `create_time`, `update_time`)
+VALUES
+    (2,'priska',100301,4,3,4,3,4,3,now(),now());
+
+/*!40000 ALTER TABLE `raffle_activity_account` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
